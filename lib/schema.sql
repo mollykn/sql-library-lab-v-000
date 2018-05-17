98d61d319039d795885b4e9d7a5e741dc765240b
Molly N <mollykathrynnelson@gmail.com>
2018-05-17 23:50:11 +0000

    automatically backed up by learn

--- lib/schema.sql
+++ lib/schema.sql
@@ -16,3 +16,10 @@ title TEXT,
 year INTEGER,
 series_id INTEGER
 );
+
+CREATE TABLE characters (id INTEGER PRIMARY KEY,
+name TEXT,
+motto TEXT,
+species TEXT,
+author_id INTEGER,
+series_id INTEGER);
