f43423cd00080a7824121f45c4bef94d1cbd1df2
Molly N <mollykathrynnelson@gmail.com>
2018-05-18 00:04:08 +0000

    automatically backed up by learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -7,3 +7,10 @@ VALUES
 INSERT INTO subgenres (id, name) VALUES
 (1, 'fantasy'),
 (2, 'fantasy');
+
+INSERT INTO authors (id, name) 
+VALUES
+(1, 'JK Rowling'),
+(2, 'CS Lewis');
+
+INSERT INTO books
