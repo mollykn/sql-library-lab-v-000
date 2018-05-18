2042e711a2967d4dab2706888468272361f68160
Molly N <mollykathrynnelson@gmail.com>
2018-05-18 00:07:48 +0000

    automatically backed up by learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -14,11 +14,10 @@ VALUES
 (2, 'CS Lewis');
 
 INSERT INTO books (id, title, year, series_id)
-VALUES 
+VALUES
 (1, 'Goblet of Fire', 2006, 1),
 (2, 'Half Blood Prince', 2005, 1),
 (3, 'Chamber of Secrets', 1999, 1),
 (4, 'Prince Caspian', 1951, 2),
 (5, 'Lion Witch Wardrobe' 1950, 2),
 (6, 'The Last Battle',1956, 2);
-
