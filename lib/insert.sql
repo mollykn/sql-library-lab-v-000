b61496c737b910b10689934e050226aa10fa7395
Molly N <mollykathrynnelson@gmail.com>
2018-05-18 00:24:22 +0000

    automatically backed up by learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,1 +1,10 @@
 INSERT INTO series (id, title, author_id, subgenre_id) VALUES (1, "Harry Potter", 1, 1), (2, "Chronicles of Narnia", 2, 2);
+
+INSERT INTO books (id, title, year, series_id)
+VALUES
+(1, 'Goblet of Fire', 2006, 1),
+(2, 'Half Blood Prince', 2005, 1),
+(3, 'Chamber of Secrets', 1999, 1),
+(4, 'Prince Caspian', 1951, 2),
+(5, 'Lion Witch Wardrobe', 1950, 2),
+(6, 'The Last Battle',1956, 2);
