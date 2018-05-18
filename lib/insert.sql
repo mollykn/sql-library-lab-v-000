d94ea51d49b9027a0c1b03a925d3073c68472c05
Molly N <mollykathrynnelson@gmail.com>
2018-05-18 00:07:43 +0000

    automatically backed up by learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -8,9 +8,17 @@ INSERT INTO subgenres (id, name) VALUES
 (1, 'fantasy'),
 (2, 'fantasy');
 
-INSERT INTO authors (id, name) 
+INSERT INTO authors (id, name)
 VALUES
 (1, 'JK Rowling'),
 (2, 'CS Lewis');
 
-INSERT INTO books
+INSERT INTO books (id, title, year, series_id)
+VALUES 
+(1, 'Goblet of Fire', 2006, 1),
+(2, 'Half Blood Prince', 2005, 1),
+(3, 'Chamber of Secrets', 1999, 1),
+(4, 'Prince Caspian', 1951, 2),
+(5, 'Lion Witch Wardrobe' 1950, 2),
+(6, 'The Last Battle',1956, 2);
+
