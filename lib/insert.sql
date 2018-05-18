9bf5943225e7983c8644496d2f633039bbf38b71
Molly N <mollykathrynnelson@gmail.com>
2018-05-18 00:29:29 +0000

    automatically backed up by learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -9,8 +9,8 @@ VALUES
 (5, 'Lion Witch Wardrobe', 1950, 2),
 (6, 'The Last Battle',1956, 2);
 
-INSERT INTO characters (id, name, motto, species, author_id, series_id) VALUES 
+INSERT INTO characters (id, name, motto, species, author_id, series_id) VALUES
 (1, 'Harry Potter', 'Lord voldemort killed my parents', 'human', 1, 1),
-(2, "Tyrion Lanister", "A Lanister always pays is debts", "human", 1, 1), 
-(3, "Daenerys Targaryen", "If I look back I am lost", "human", 1, 1), 
-(4, "Eddard Stark", "Winter is coming", "human", 1, 1);
+((2, 'Hermoine', 'You idiot ron', 'human', 1, 1),
+(3, 'Buckbeak', 'Cawww caww', 'hippogriff', 1, 1),
+(4, 'Fleru Delacour', 'oui oui', 'half veela', 1, 1);
