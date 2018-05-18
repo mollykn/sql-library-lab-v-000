26979030aed412192adc197432103e6e5fb79e8f
Molly N <mollykathrynnelson@gmail.com>
2018-05-18 00:08:06 +0000

    automatically backed up by learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -19,5 +19,5 @@ VALUES
 (2, 'Half Blood Prince', 2005, 1),
 (3, 'Chamber of Secrets', 1999, 1),
 (4, 'Prince Caspian', 1951, 2),
-(5, 'Lion Witch Wardrobe' 1950, 2),
+(5, 'Lion Witch Wardrobe', 1950, 2),
 (6, 'The Last Battle',1956, 2);
