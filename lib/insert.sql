f78284b594f191390ca81863671075e72893572a
Molly N <mollykathrynnelson@gmail.com>
2018-05-18 00:27:29 +0000

    automatically backed up by learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -15,3 +15,9 @@ VALUES
 (2, 'Hermoine', 'You idiot Ron', 'human', 1, 1),
 (3, 'Buckbeak', 'Cawww caww', 'hippogriff', 1, 1),
 (4, 'Fleru Delacour', 'oui oui', 'half veela', 1, 1);
+
+INSERT INTO characters (id, name, motto, species, author_id, series_id),
+(5, 'Lucy', 'what is this', 'human', 2, 2),
+(6, 'Aslan', 'Im here now do not worry', 'deity', 2, 2),
+(7, 'Caspian', 'ahoy', 'human', 2, 2),
+(8, 'Mr. tumnus', 'watch out', 'faun', 2, 2);
