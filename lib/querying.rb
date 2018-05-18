2f1bdc57752d28a253fb95c062c465674c37a87c
Molly N <mollykathrynnelson@gmail.com>
2018-05-18 01:05:21 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -22,9 +22,9 @@ def select_name_and_series_subgenres_of_authors
   "SELECT authors.name, subgenres.name
   FROM authors
   JOIN series
-  ON series.author_id = author_id
+  ON series.author_id = authors_id
   JOIN subgenres
-  ON series.subgenre_id = subgenre_id; "
+  ON series.subgenre_id = subgenres_id; "
 end
 
 def select_series_title_with_most_human_characters
