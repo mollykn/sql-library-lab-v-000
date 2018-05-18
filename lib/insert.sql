97c8291798824ecead75ff07d06027ff8a6e3da6
Molly N <mollykathrynnelson@gmail.com>
2018-05-18 00:03:16 +0000

    automatically backed up by learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,2 +1,9 @@
-INSERT INTO series (id, title, author_id, subgenre_id)
-VALUES (1, 'Harry Potter', 1, 1), (2, 'Chronicles of Narnia', 2, 2);
+INSERT INTO series
+(id, title, author_id, subgenre_id)
+VALUES
+(1, 'Harry Potter', 1, 1),
+(2, 'Chronicles of Narnia', 2, 2);
+
+INSERT INTO subgenres (id, name) VALUES
+(1, 'fantasy'),
+(2, 'fantasy');
