7e4714313145bb3c7d5b3c35a7e9862202576e9e
Molly N <mollykathrynnelson@gmail.com>
2018-05-18 01:23:54 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -32,11 +32,14 @@ def select_series_title_with_most_human_characters
   FROM series
   JOIN books
   ON books.series_id = series.id
-
-
-
-
-
+  JOIN character_books
+  ON character_books.book_id = books.id
+  JOIN characters
+  ON character_books.book_id = characters.id
+  WHERE characters.species = "human",
+  GROUP BY series.title
+  ORDER BY COUNT(*) DESC
+  LIMIT 1;
   "
 end
 
